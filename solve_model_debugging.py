e540a79f6ad669a75398eb227fd7c19a71c49979
madsankern <60843185+madsankern@users.noreply.github.com>
2021-04-28 13:13:24 +0200

    Update solve_model_debugging.py

--- solve_model_debugging.py
+++ solve_model_debugging.py
@@ -1,3 +1,9 @@
+####################
+## Debugging file ##
+####################
+# Debugging file where we can run all our solvers without going through the notebook.
+# Call any solver you want to check externally here
+
 # Load packages
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,7 +15,11 @@ import model
 import vfi
 import egm
 
-# Setup
+# Import paramteters
 par = model.setup()
 
 egm = egm.solve_EGM_2d(par)
+
+vfi = vfi.solve_VFI_2d(par)
+
+fd = fd.solve_fd(par)
