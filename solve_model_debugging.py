eddf5bd24b1a78dfdc95712237ab023e4475abf4
madsankern <jqk218@econ.ku.dk>
2021-04-28 13:10:45 +0200

    Update

--- solve_model_debugging.py
+++ solve_model_debugging.py
@@ -12,13 +12,4 @@ import egm
 # Setup
 par = model.setup()
 
-# # Solve simple model with VFI
-# sol_vfi = vfi.solve_VFI(par)
-
-# # Solve simple model EGM
-# sol_egm = egm.solve_EGM(par)
-
-# Backup version
-# sol_egm_2d = egm.solve_EGM_2d(par)
-
 egm = egm.solve_EGM_2d(par)
