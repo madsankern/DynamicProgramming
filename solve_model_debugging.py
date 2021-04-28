261a82564fe33b11762e5a52c38f94441d3710ae
madsankern <jqk218@econ.ku.dk>
2021-04-28 15:23:58 +0200

    Update

--- solve_model_debugging.py
+++ solve_model_debugging.py
@@ -4,22 +4,34 @@
 # Debugging file where we can run all our solvers without going through the notebook.
 # Call any solver you want to check externally here
 
-# Load packages
+# Imports and settings
+%load_ext autoreload
+%autoreload 2
+%matplotlib inline
+
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('seaborn-whitegrid')
 
-# load user written modules
 import tools
 import model
 import vfi
 import egm
+import fd
 
-# Import paramteters
 par = model.setup()
 
-egm = egm.solve_EGM_2d(par)
+# egm = egm.solve_EGM_2d(par)
+sol_vfi = vfi.solve_VFI(par)
+# fd = fd.solve_fd(par)
 
-vfi = vfi.solve_VFI_2d(par)
+# # Plotting
+# fig = plt.figure(figsize=(8,6))
+# ax = fig.add_subplot(1,1,1)
 
-fd = fd.solve_fd(par)
+# ax.plot(vfi.a, vfi.c[0,:], linestyle = ':', color = 'red', label = '$y_1$')
+# ax.plot(vfi.a, vfi.c[1,:], linestyle = ':', color = 'blue', label = '$y_2$')
+# ax.set_xlim([-1,20])
+# ax.legend(frameon=True)
+
+# plt.plot()
