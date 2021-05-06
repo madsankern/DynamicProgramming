bfcd2e6b0194be960505c1aaf7124cbaaf1009b5
madsankern <jqk218@econ.ku.dk>
2021-05-06 09:41:25 +0200

    Added solve function

--- egm.py
+++ egm.py
@@ -1,11 +1,40 @@
 # Solve problem using endogneous grid method
-# General kommentar: Først og fremmest, insane arbejde! Jeg synes dog vi bør overveje, at splitte funktionerne op, så
-# EGM-funktionerne kun omhandler for-loopet, der itererer igennem end-of-period assets, og while-loopet i stedet bliver kaldt
+# General kommentar: Først og  fremmest, insane arbejde! Jeg synes dog vi bør overveje, at splitte funktionerne op, så
+# EGM-funktionerne kun omhandler for-loopet, der itererer igennem end-of-periodassets, og while-loopet i stedet bliver kaldt
 # i en solver-funktion i filen "model.py", da vi på den måde adskiller hvad der er EGM og hvad der er contraction mapping.  
 
 import numpy as np
 import tools
 import utility as util
+
+def solve(sol, par, c_next, m_next):
+
+    # Loop over exogneous states (post decision states)
+    for a_i,a in enumerate(par.grid_a):
+        
+        #Next periods assets and consumption
+        m_plus = (1+par.r)*a + np.transpose(par.y) # Transpose for dimension to fit
+
+        # Interpolate next periods consumption - can this be combined?
+        c_plus_1 = tools.interp_linear_1d(m_next[0,:], c_next[0,:], m_plus) # State 1
+        c_plus_2 = tools.interp_linear_1d(m_next[1,:], c_next[1,:], m_plus) # State 2
+
+        #Combine into a vector. Rows indicate income state, columns indicate asset state
+        c_plus = np.vstack((c_plus_1, c_plus_2))
+
+        # Marginal utility
+        marg_u_plus = util.marg_u(c_plus,par)
+        av_marg_u_plus = np.sum(par.P*marg_u_plus, axis = 1) # Dot product by row (axis = 1)
+
+        # Add optimal consumption and endogenous state
+        sol.c[:,a_i+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
+        sol.m[:,a_i+1] = a + sol.c[:,a_i+1]
+
+    # Update iteration parameters
+    sol.it += 1
+    sol.delta = max( max(abs(sol.c[0] - c_next[0])), max(abs(sol.c[1] - c_next[1]))) # check this, is this optimal
+
+    return sol
 
 def solve_EGM(par):
     
@@ -30,11 +59,11 @@ def solve_EGM(par):
         c_next = sol.c.copy()
         a_next = sol.a.copy()
         # Loop over exogneous states (post decision states)
-        for i_s,s in enumerate(par.grid_s):
+        for a_i,s in enumerate(par.grid_s):
             
             #Next periods assets and consumption
-            a_plus = (1+par.r)*s + par.y # post decision state. Note vector
-            c_plus = tools.interp_linear_1d(a_next, c_next, a_plus)
+            m_plus = (1+par.r)*s + par.y # post decision state. Note vector
+            c_plus = tools.interp_linear_1d(a_next, c_next, m_plus)
             
             # Marginal utility of next periods consumption
             marg_u_plus = util.marg_u(c_plus, par)
@@ -42,8 +71,8 @@ def solve_EGM(par):
 
             # Optimal c in current period from inverted euler
             # +1 in indexation as we add zero consumption afterwards
-            sol.c[i_s+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
-            sol.a[i_s+1] = s + sol.c[i_s+1] # Endogenous state
+            sol.c[a_i+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
+            sol.a[a_i+1] = s + sol.c[a_i+1] # Endogenous state
             
         # add zero consumption
         sol.a[0] = 0
@@ -84,14 +113,14 @@ def solve_EGM_2d(par):
         a_next = sol.a.copy()
 
         # Loop over exogneous states (post decision states)
-        for i_s,s in enumerate(par.grid_s):
+        for a_i,s in enumerate(par.grid_s):
             
             #Next periods assets and consumption
-            a_plus = (1+par.r)*s + np.transpose(par.y) # Transpose for dimension to fit
+            m_plus = (1+par.r)*s + np.transpose(par.y) # Transpose for dimension to fit
 
             # Interpolate next periods consumption - can this be combined?
-            c_plus_1 = tools.interp_linear_1d(a_next[0,:], c_next[0,:], a_plus) # State 1
-            c_plus_2 = tools.interp_linear_1d(a_next[1,:], c_next[1,:], a_plus) # State 2
+            c_plus_1 = tools.interp_linear_1d(a_next[0,:], c_next[0,:], m_plus) # State 1
+            c_plus_2 = tools.interp_linear_1d(a_next[1,:], c_next[1,:], m_plus) # State 2
 
             #Combine into a vector. Rows indicate income state, columns indicate asset state
             c_plus = np.vstack((c_plus_1, c_plus_2))
@@ -101,8 +130,8 @@ def solve_EGM_2d(par):
             av_marg_u_plus = np.sum(par.P*marg_u_plus, axis = 1) # Dot product by row (axis = 1)
 
             # Add optimal consumption and endogenous state
-            sol.c[:,i_s+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
-            sol.a[:,i_s+1] = s + sol.c[:,i_s+1]
+            sol.c[:,a_i+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
+            sol.a[:,a_i+1] = s + sol.c[:,a_i+1]
        
         # Update iteration parameters
         sol.it += 1
