3b02a43ae4920c336fc4cb648cf70ada0242cc47
madsankern <jqk218@econ.ku.dk>
2021-05-08 09:31:18 +0200

    Attempt at vectorizing

--- egm.py
+++ egm.py
@@ -8,7 +8,13 @@ import utility as util
 
 def solve(sol, par, c_next, m_next):
 
+    # Copy last iteration of the value function
     v_old = sol.v.copy()
+
+    # Expand exogenous asset grid
+    a = np.tile(par.grid_a, np.size(par.y)) # does this work?
+
+    m_plus = (1+par.r)
 
     # Loop over exogneous states (post decision states)
     for a_i,a in enumerate(par.grid_a):
@@ -39,6 +45,83 @@ def solve(sol, par, c_next, m_next):
     # sol.delta = max( max(abs(sol.c[0] - c_next[0])), max(abs(sol.c[1] - c_next[1])))
 
     return sol
+
+
+## Attempt at vectorizing the code
+# def solve_vec(sol, par, c_next, m_next):
+
+#     # Copy last iteration of the value function
+#     v_old = sol.v.copy()
+
+#     # Expand exogenous asset grid
+#     shape = (np.size(par.y),1)
+
+
+#     a = np.tile(par.grid_a, shape) # does this work?
+
+#     y_help = np.array([par.y])
+#     y_help = np.transpose(y_help)
+#     y = np.tile(y_help, (1,par.Na))
+
+#     m_plus = (1+par.r)*a + y
+#     # m_plus = (1+par.r)*a + np.transpose(par.y)
+
+#     # Interpolate next periods consumption
+#     c_plus_1 = tools.interp_linear_1d(m_next[0,:], c_next[0,:], m_plus[0,:]) # State 1
+#     c_plus_2 = tools.interp_linear_1d(m_next[1,:], c_next[1,:], m_plus[1,:]) # State 2
+
+#     #Combine into a vector. Rows indicate income state, columns indicate asset state
+#     c_plus = np.vstack((c_plus_1, c_plus_2))
+
+#     # Marginal utility
+#     marg_u_plus = util.marg_u(c_plus,par)
+#     av_marg_u_plus = np.sum(par.P*marg_u_plus)
+
+#     av_marg_u_plus = np.sum(par.P*marg_u_plus, axis = 1) # Dot product by row (axis = 1)
+
+#     # Add optimal consumption and endogenous state
+#     sol.c = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
+#     sol.m = a + sol.c[:,a_i+1]
+#     sol.v = util.u(sol.c,par)
+
+
+
+    # Loop over exogneous states (post decision states)
+    for a_i,a in enumerate(par.grid_a):
+        
+        #Next periods assets and consumption
+        m_plus = (1+par.r)*a + np.transpose(par.y) # Transpose for dimension to fit
+
+        # Interpolate next periods consumption - can this be combined?
+        c_plus_1 = tools.interp_linear_1d(m_next[0,:], c_next[0,:], m_plus) # State 1
+        c_plus_2 = tools.interp_linear_1d(m_next[1,:], c_next[1,:], m_plus) # State 2
+
+        #Combine into a vector. Rows indicate income state, columns indicate asset state
+        c_plus = np.vstack((c_plus_1, c_plus_2))
+
+        # Marginal utility
+        marg_u_plus = util.marg_u(c_plus,par)
+        av_marg_u_plus = np.sum(par.P*marg_u_plus, axis = 1) # Dot product by row (axis = 1)
+
+        # Add optimal consumption and endogenous state
+        sol.c[:,a_i+1] = util.inv_marg_u((1+par.r)*par.beta*av_marg_u_plus,par)
+        sol.m[:,a_i+1] = a + sol.c[:,a_i+1]
+        sol.v = util.u(sol.c,par)
+
+    #Compute valu function and update iteration parameters
+    sol.delta = max( max(abs(sol.v[0] - v_old[0])), max(abs(sol.v[1] - v_old[1])))
+    sol.it += 1
+
+    # sol.delta = max( max(abs(sol.c[0] - c_next[0])), max(abs(sol.c[1] - c_next[1])))
+
+    return sol
+
+
+
+
+
+
+
 
 
 
