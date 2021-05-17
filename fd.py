001974ac628039733d1b9200bfefc3a663a44558
madsankern <jqk218@econ.ku.dk>
2021-05-17 09:23:50 +0200

    Update

--- fd.py
+++ fd.py
@@ -1,7 +1,3 @@
-##############################
-## Finite Difference method ##
-##############################
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import sparse
@@ -9,6 +5,10 @@ from scipy.sparse.linalg import spsolve
 
 # Local modules
 import utility as util
+
+##############################
+## Finite Difference method ##
+##############################
 
 def solve(par,sol):
 
@@ -20,7 +20,7 @@ def solve(par,sol):
     # Dimension of transition matrix
     n = par.Nm * y_size 
 
-    # Initial guess - zero savings
+    # Initial guess - zero savings - add consumption explicitly
     sol.v = util.u(np.tile(par.grid_a,(y_size,1))*par.r
                     + np.tile(y_vals,(par.Nm,1)).transpose(),par)/par.rho
 
@@ -52,7 +52,8 @@ def solve(par,sol):
     while (sol.delta >= par.tol_fd and sol.it < par.max_iter):
 
         dv = (sol.v[:,1:]-sol.v[:,:-1])/dm # Finite difference approx
-        c_foc = util.inv_marg_u(dv,par) # Optimal consumption given value function
+
+        c_foc = np.maximum(util.inv_marg_u(dv,par),1e-4) # Optimal consumption given value function
         c0 = np.tile(par.grid_m,(y_size,1))*par.r \
                         + np.tile(y_vals,(par.Nm,1)).transpose() # Income
 
@@ -64,15 +65,16 @@ def solve(par,sol):
         is_forward = ssf > 0
         is_backward = ssb < 0
 
-        # Update savings based on forward or backward difference based on direction of drift
+        # Compute consumption using upwind scheme
         c0[:,:-1] += (c_foc - c0[:,:-1])*is_forward[:,:-1]
         c0[:,1:] += (c_foc - c0[:,1:])*is_backward[:,1:]
-            
+
         # UNCOMMENT FOR DEBUGGING
         #plt.plot(self.par.grid_m, self.c0.transpose())#
         #plt.show()
 
-        c0 = util.u(c0,par) # Check this out
+        #c0 = util.u(c0,par) # Check this out
+        u_vec = util.u(c0,par)
        
         # Build the A matrix
         sol.A = y_transition.copy() # Transition matrix containing lambdas
@@ -87,7 +89,7 @@ def solve(par,sol):
 
         # Solve the system of linear equations corresponding to implicit finite difference scheme
         sol.B = sparse.eye(n)*(1/Delta + par.rho) - sol.A
-        b = c0.reshape(n,1) + sol.v.reshape(n,1)/Delta
+        b = u_vec.reshape(n,1) + sol.v.reshape(n,1)/Delta
         v_old = sol.v.copy() # Save for comparison
         sol.v = spsolve(sol.B,b).reshape(y_size,par.Nm)
 
@@ -96,8 +98,8 @@ def solve(par,sol):
         sol.it += 1
 
     # Unpack solution - Add what is needed here
-    sol.c = c_foc
-    sol.a = par.grid_a
+    sol.c = c0
+    sol.m = par.grid_m
     sol.dv = dv
 
     return sol
