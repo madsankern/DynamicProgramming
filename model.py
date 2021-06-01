f333136ec3ea1864a901441748db701c3c5b1cb5
Wind <Wind.Mads@bcg.com>
2021-06-01 16:28:14 +0200

    Minor changes

--- model.py
+++ model.py
@@ -165,17 +165,22 @@ class model_class():
             # therefore, copy c and a grid from last iteration.
             c_next = sol.c.copy()
             m_next = sol.m.copy()
+            
 
             # Call EGM function here
             sol = egm.solve(sol, par, c_next, m_next)
+
+            # add zero consumption (not really necessary for current initial guess, where everything is consumed)
+            # sol.m[:,0] = 1e-4
+            # sol.c[:,0] = 1e-4
 
             #Compute value function and update iteration parameters [is currently coded inside the EGM step]
             #sol.delta = max( max(abs(sol.v[0] - v_old[0])), max(abs(sol.v[1] - v_old[1])))
             #sol.it += 1
 
         # add zero consumption (not really necessary for current initial guess, where everything is consumed)
-        sol.m[:,0] = 0.0
-        sol.c[:,0] = 0.0
+        # sol.m[:,0] = 1e-4
+        # sol.c[:,0] = 1e-4
 
     ##############################
     ## Finite difference method ##
