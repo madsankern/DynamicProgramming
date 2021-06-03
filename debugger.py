591b684637afa2909f36d07a09b676a65d9e173b
nbm205 <77975806+nbm205@users.noreply.github.com>
2021-06-03 23:17:52 +0300

    Fixed expectation in VFI, and shortened upper-envelope
    .

--- debugger.py
+++ debugger.py
@@ -14,5 +14,13 @@ sol = model.sol_egm
 par = model.par
 
 # Plot
-plt.plot(par.grid_m,sol.h[0])
-plt.scatter(par.grid_m,sol.c[1])
+# plt.plot(par.grid_m,sol.h[0])
+# plt.scatter(par.grid_m,sol.c[1])
+
+fig = plt.figure(figsize=(6,4))
+ax = fig.add_subplot(1,1,1)
+
+ax.plot(model.sol_egm.m[0,:],model.sol_egm.c[0,:], label= r'Not having a house', linestyle = '-', color = '0.4')
+ax.plot(model.sol_egm.m[1,:],model.sol_egm.c[1,:], label= r'Having a house', linestyle = '--', color = '0.4')
+
+plt.show()
